82be599deeb3b151da16480000f68084896670a1
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-08-01 21:34:04 +0530

    🔥 | remove init function

--- app/src/utils/wallet.js
+++ app/src/utils/wallet.js
@@ -26,30 +26,6 @@ BigInt.prototype.toJSON = function () {
   return this.toString()
 }
 
-export async function init(setConnectedAccount) {
-  const client = await detectConcordiumProvider()
-  // Listen for relevant events from the wallet.
-  client.on('accountChanged', (account) => {
-    console.debug('browserwallet event: accountChange', { account })
-    setConnectedAccount(account)
-  })
-  client.on('accountDisconnected', () => {
-    console.debug('browserwallet event: accountDisconnected')
-    client.getMostRecentlySelectedAccount().then(setConnectedAccount)
-  })
-  client.on('chainChanged', (chain) => {
-    console.debug('browserwallet event: chainChanged', { chain })
-  })
-  client.getMostRecentlySelectedAccount().then(setConnectedAccount)
-
-  return client
-}
-
-export async function connect(client, setConnectedAccount) {
-  const account = await client.connect()
-  return setConnectedAccount(account)
-}
-
 // Check if the user is connected to the testnet chain by checking if the testnet genesisBlock exists.
 async function checkConnectedToTestnet(client) {
   return client
@@ -174,7 +150,6 @@ export async function createProposal(
       AccountTransactionType.Update,
       {
         amount: CcdAmount.fromMicroCcd(BigInt(0)),
-        //Check whehter we need amount as it is not needed in our contract since we not transferring any tokens
         address: { index: BigInt(CONTRACT_INDEX), subindex: BigInt(0) },
         receiveName: 'DAO.create_proposal',
         maxContractExecutionEnergy: BigInt(30000),
@@ -190,13 +165,7 @@ export async function createProposal(
   }
 }
 
-export async function renounceVotes(
-  client,
-  contractIndex,
-  proposalID,
-  votes,
-  senderAddress,
-) {
+export async function renounceVotes(client, proposalID, votes, senderAddress) {
   const connectedToTestnet = await checkConnectedToTestnet(client)
   if (connectedToTestnet) {
     const txHash = await client.sendTransaction(
@@ -204,9 +173,8 @@ export async function renounceVotes(
       AccountTransactionType.Update,
       {
         amount: CcdAmount.fromMicroCcd(BigInt(0)),
-        //Check whehter we need amount as it is not needed in our contract since we not transferring any tokens
-        address: { index: BigInt(contractIndex), subindex: BigInt(0) },
-        receiveName: 'DAO.create_proposal',
+        address: { index: BigInt(CONTRACT_INDEX), subindex: BigInt(0) },
+        receiveName: 'DAO.renounce',
         maxContractExecutionEnergy: BigInt(30000),
       },
       {
@@ -240,7 +208,6 @@ export async function insertFunds(client, amount, senderAddress) {
 
 export async function voteForProposal(
   client,
-  contractIndex,
   proposalID,
   vote_decision,
   senderAddress,
@@ -251,14 +218,14 @@ export async function voteForProposal(
       senderAddress,
       AccountTransactionType.Update,
       {
-        // amount: CcdAmount.fromMicroCcd(BigInt(0)),
-        address: { index: BigInt(contractIndex), subindex: BigInt(0) },
+        amount: CcdAmount.fromMicroCcd(BigInt(0)),
+        address: { index: BigInt(CONTRACT_INDEX), subindex: BigInt(0) },
         receiveName: 'DAO.vote',
         maxContractExecutionEnergy: BigInt(30000),
       },
       {
         proposal_id: proposalID,
-        vote_for: vote_decision,
+        votes: BigInt(vote_decision),
       },
       RAW_SCHEMA_BASE64,
     )
@@ -267,25 +234,20 @@ export async function voteForProposal(
   }
 }
 
-export async function withdrawFunds(
-  client,
-  contractIndex,
-  proposalID,
-  senderAddress,
-) {
+export async function withdrawFunds(client, proposalID, senderAddress) {
   const connectedToTestnet = await checkConnectedToTestnet(client)
   if (connectedToTestnet) {
     const txHash = await client.sendTransaction(
       senderAddress,
       AccountTransactionType.Update,
       {
-        // amount: CcdAmount.fromMicroCcd(BigInt(0)),
-        address: { index: BigInt(contractIndex), subindex: BigInt(0) },
+        amount: CcdAmount.fromMicroCcd(BigInt(0)),
+        address: { index: BigInt(CONTRACT_INDEX), subindex: BigInt(0) },
         receiveName: 'DAO.withdraw',
         maxContractExecutionEnergy: BigInt(30000),
       },
       {
-        proposalID, //Need to check because it is not having any key in the schema.bin file so we have to pass only proposalID or with header need to work on it.
+        proposal_id: proposalID,
       },
       RAW_SCHEMA_BASE64,
     )
@@ -316,7 +278,7 @@ export async function getPower(client, account) {
   let param = serializeUpdateContractParameters(
     ContractName.fromString(CONTRACT_NAME),
     EntrypointName.fromString('get_power'),
-    account,
+    { address: account },
     toBuffer(RAW_SCHEMA_BASE64, 'base64'),
     SchemaVersion.V2,
     true,
@@ -325,7 +287,7 @@ export async function getPower(client, account) {
   const res = await client.getGrpcClient().invokeContract({
     contract: { index: BigInt(CONTRACT_INDEX), subindex: BigInt(0) },
     method: 'DAO.get_power',
-    paramter: param,
+    parameter: param,
   })
 
   console.log('SSS', res)
