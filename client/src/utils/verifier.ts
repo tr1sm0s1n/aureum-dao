bb776acb545a32566d7caa15f5acf8716da2a206
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-09-06 16:15:01 +0530

    🔥 | remove verifier url

--- client/src/utils/verifier.ts
+++ client/src/utils/verifier.ts
@@ -1,15 +1,12 @@
-import { VERIFIER_URL } from '../config/config'
 import { Challenge, ProofWithContext } from '../types'
 
-const verifier = VERIFIER_URL
 /**
  * Fetch a challenge from the backend
  */
 export async function getChallenge(accountAddress: string) {
-  const response = await fetch(
-    `${verifier}/challenge?address=` + accountAddress,
-    { method: 'get' }
-  )
+  const response = await fetch(`/challenge?address=` + accountAddress, {
+    method: 'get',
+  })
   const body = await response.json()
   return body.challenge
 }
@@ -18,11 +15,9 @@ export async function getChallenge(accountAddress: string) {
  * Fetch the statement to prove from the backend
  */
 export async function getStatement() {
-  const response = await fetch(`${verifier}/statement`, { method: 'get' })
-  // console.log("inside getstatement- res", response)
+  const response = await fetch(`/statement`, { method: 'get' })
   const body = await response.json()
   const bodyString = JSON.stringify(body)
-  // console.log("bodystr", bodyString)
   return JSON.parse(bodyString)
 }
 
@@ -30,7 +25,7 @@ export async function getStatement() {
  *  Authorize with the backend, and get a auth token.
  */
 export async function authorize(challenge: Challenge, proof: ProofWithContext) {
-  const response = await fetch(`${verifier}/prove`, {
+  const response = await fetch(`/prove`, {
     method: 'post',
     headers: new Headers({ 'content-type': 'application/json' }),
     body: JSON.stringify({ challenge, proof }),
